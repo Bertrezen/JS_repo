ed4fe0a38a96ca644f05ee36820b4b6b96898d0f
Betrezen <wachteram@gmail.om>
2021-02-09 12:45:41 +0400

    answers

--- Lesson3/Question1.js
+++ Lesson3/Question1.js
@@ -1,24 +1,24 @@
 //      Создаем массив от 1 до 100
 var arr = [];
-var end = 1;
-while (end < 100){
+var end = 1; // 0 и 1 не являются ростыми числами
+while (end < 100) {
     end++;
     arr.push(end);
 }
 //      Удаляем все четные цифры
 var a = 2;
 var b = 2;
-while (a <= 100){
+while (a <= 100) {
     a += b;
-        arr.splice(arr.indexOf(a), 1);
+    arr.splice(arr.indexOf(a), 1);
 }
 //      Удаляет все цифры которые деляться на нечетные до 10
 a = 4;
 b = 3;
-while (a < 100){
-    if (a % b ===0 && arr.indexOf(a) !== -1){
+while (a < 100) {
+    if (a % b === 0 && arr.indexOf(a) !== -1) {
         arr.splice(arr.indexOf(a), 1);
-    }else {
+    } else {
         a++;
     }
 }
@@ -44,3 +44,19 @@ while (a < 100) {
 }
 
 console.log(arr);
+
+console.log('======== 2 вариант ========');
+
+let nums = 2; // 0 и 1 не являются ростыми числами
+
+while (nums <= 100) {
+    let check = true;
+    for (let i = 2; i < nums; i++) {
+        if (nums % i === 0) {
+            check = false;
+            break;
+        }
+    }
+    if (check) console.log(nums);
+    nums++;
+}
